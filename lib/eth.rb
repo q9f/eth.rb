9a69ca9626315fb39c17795f2c4fa7f9ec05ddd6
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2021-11-29 15:59:03 +0100

    lib: reset greatly

--- lib/eth.rb
+++ lib/eth.rb
@@ -1,69 +1,17 @@
-require 'digest/sha3'
-require 'ffi'
-require 'money-tree'
-require 'rlp'
+# Copyright (c) 2016-2021 Steve Ellis
+# Copyright (c) 2021-2022 Afri Schoedon
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 module Eth
-  BYTE_ZERO = "\x00".freeze
-  UINT_MAX = 2**256 - 1
-
-  autoload :Address, 'eth/address'
-  autoload :Gas, 'eth/gas'
-  autoload :Key, 'eth/key'
-  autoload :OpenSsl, 'eth/open_ssl'
-  autoload :Secp256k1, 'eth/secp256k1'
-  autoload :Sedes, 'eth/sedes'
-  autoload :Tx, 'eth/tx'
-  autoload :Utils, 'eth/utils'
-
-  class << self
-    def configure
-      yield(configuration)
-    end
-
-    def replayable_chain_id
-      27
-    end
-
-    def chain_id
-      configuration.chain_id
-    end
-
-    def v_base
-      replayable_chain_id
-    end
-
-    def replayable_v?(v)
-      [replayable_chain_id, replayable_chain_id + 1].include? v
-    end
-
-    def tx_data_hex?
-      !!configuration.tx_data_hex
-    end
-
-    def chain_id_from_signature(signature)
-      return nil if Eth.replayable_v?(signature[:v])
-
-      cid = (signature[:v] - 35) / 2
-      (cid < 1) ? nil : cid
-    end
-
-    private
-
-    def configuration
-      @configuration ||= Configuration.new
-    end
-  end
-
-  class Configuration
-    attr_accessor :chain_id, :tx_data_hex
-
-    def initialize
-      self.chain_id = nil
-      self.tx_data_hex = true
-    end
-  end
-
-  class ValidationError < StandardError; end
-  class InvalidTransaction < ValidationError; end
 end
