d800fda9be9ad77cd3950d88b3f24197eabc1b92
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-12-01 14:12:33 +0100

    ci: enable codecov

--- Gemfile
+++ Gemfile
@@ -7,7 +7,9 @@ gem 'rbsecp256k1', '~> 5.0'
 
 group :test, :development do
   gem 'bundler', '~> 2.2'
+  gem 'codecov', '~> 0.6'
   gem 'pry', '~> 0.14'
   gem 'rake', '~> 13.0'
   gem 'rspec', '~> 3.10'
+  gem 'simplecov', '~> 0.21'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require 'eth'
+require 'simplecov'
+SimpleCov.start
 
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
+require 'eth'
 RSpec.configure do |config|
 end
