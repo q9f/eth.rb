ca74fcadadc954239754087da31d457c04545262
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-12-02 18:21:28 +0100

    lib: add more docs to utils

--- spec/eth/key_spec.rb
+++ spec/eth/key_spec.rb
@@ -14,14 +14,14 @@ describe Eth::Key do
       expect(bob).to be_an_instance_of Eth::Key
 
       # ensure both keys are not the same
-      expect(alice.private_key).not_to eq(bob.private_key)
-      expect(alice.private_hex).not_to eq(bob.private_hex)
-      expect(alice.private_bytes).not_to eq(bob.private_bytes)
-      expect(alice.public_key).not_to eq(bob.public_key)
-      expect(alice.public_hex).not_to eq(bob.public_hex)
-      expect(alice.public_hex_compressed).not_to eq(bob.public_hex_compressed)
-      expect(alice.public_bytes).not_to eq(bob.public_bytes)
-      expect(alice.public_bytes_compressed).not_to eq(bob.public_bytes_compressed)
+      expect(alice.private_key).not_to eq bob.private_key
+      expect(alice.private_hex).not_to eq bob.private_hex
+      expect(alice.private_bytes).not_to eq bob.private_bytes
+      expect(alice.public_key).not_to eq bob.public_key
+      expect(alice.public_hex).not_to eq bob.public_hex
+      expect(alice.public_hex_compressed).not_to eq bob.public_hex_compressed
+      expect(alice.public_bytes).not_to eq bob.public_bytes
+      expect(alice.public_bytes_compressed).not_to eq bob.public_bytes_compressed
     end
 
     it "restores keypairs from existing private keys" do
@@ -30,14 +30,14 @@ describe Eth::Key do
       backup = Eth::Key.new priv: alice.private_key.data
 
       # ensure both keys are the same
-      expect(alice.private_key).to eq(backup.private_key)
-      expect(alice.private_hex).to eq(backup.private_hex)
-      expect(alice.private_bytes).to eq(backup.private_bytes)
-      expect(alice.public_key).to eq(backup.public_key)
-      expect(alice.public_hex).to eq(backup.public_hex)
-      expect(alice.public_hex_compressed).to eq(backup.public_hex_compressed)
-      expect(alice.public_bytes).to eq(backup.public_bytes)
-      expect(alice.public_bytes_compressed).to eq(backup.public_bytes_compressed)
+      expect(alice.private_key).to eq backup.private_key
+      expect(alice.private_hex).to eq backup.private_hex
+      expect(alice.private_bytes).to eq backup.private_bytes
+      expect(alice.public_key).to eq backup.public_key
+      expect(alice.public_hex).to eq backup.public_hex
+      expect(alice.public_hex_compressed).to eq backup.public_hex_compressed
+      expect(alice.public_bytes).to eq backup.public_bytes
+      expect(alice.public_bytes_compressed).to eq backup.public_bytes_compressed
     end
 
     it "can handle hex and byte private keys" do
@@ -46,12 +46,12 @@ describe Eth::Key do
       backup_from_hex = Eth::Key.new priv: alice.private_hex
 
       # it should be correct no matter what format we pass through
-      expect(alice.private_key).to eq(backup_from_bytes.private_key)
-      expect(alice.private_key).to eq(backup_from_hex.private_key)
-      expect(alice.private_hex).to eq(backup_from_bytes.private_hex)
-      expect(alice.private_hex).to eq(backup_from_hex.private_hex)
-      expect(alice.private_bytes).to eq(backup_from_bytes.private_bytes)
-      expect(alice.private_bytes).to eq(backup_from_hex.private_bytes)
+      expect(alice.private_key).to eq backup_from_bytes.private_key
+      expect(alice.private_key).to eq backup_from_hex.private_key
+      expect(alice.private_hex).to eq backup_from_bytes.private_hex
+      expect(alice.private_hex).to eq backup_from_hex.private_hex
+      expect(alice.private_bytes).to eq backup_from_bytes.private_bytes
+      expect(alice.private_bytes).to eq backup_from_hex.private_bytes
     end
   end
 
@@ -67,8 +67,8 @@ describe Eth::Key do
       expect(Eth::Utils.is_hex? charlie.private_bytes).to be_falsy
 
       # check private keys are 32 bit
-      expect(charlie.private_hex.size).to eq(64)
-      expect(charlie.private_bytes.size).to eq(32)
+      expect(charlie.private_hex.size).to eq 64
+      expect(charlie.private_bytes.size).to eq 32
     end
   end
 
@@ -86,14 +86,14 @@ describe Eth::Key do
       expect(Eth::Utils.is_hex? dave.public_bytes_compressed).to be_falsy
 
       # check public key sizes and first indicator bytes
-      expect(dave.public_hex.size).to eq(130)
-      expect(dave.public_hex[0, 2]).to eq("04")
-      expect(dave.public_hex_compressed.size).to eq(66)
-      expect(dave.public_hex_compressed[0, 2]).to eq("02").or eq("03")
-      expect(dave.public_bytes.size).to eq(65)
-      expect(dave.public_bytes.bytes.first).to eq(4)
-      expect(dave.public_bytes_compressed.size).to eq(33)
-      expect(dave.public_bytes_compressed.bytes.first).to eq(2).or eq(3)
+      expect(dave.public_hex.size).to eq 130
+      expect(dave.public_hex[0, 2]).to eq "04"
+      expect(dave.public_hex_compressed.size).to eq 66
+      expect(dave.public_hex_compressed[0, 2]).to eq("02").or eq "03"
+      expect(dave.public_bytes.size).to eq 65
+      expect(dave.public_bytes.bytes.first).to eq 4
+      expect(dave.public_bytes_compressed.size).to eq 33
+      expect(dave.public_bytes_compressed.bytes.first).to eq(2).or eq 3
     end
   end
 
@@ -103,9 +103,9 @@ describe Eth::Key do
       address = '0x759b427456623a33030bbC2195439C22A8a51d25'
       private_hex = 'c3a4349f6e57cfd2cbba275e3b3d15a2e4cf00c89e067f6e05bfee25208f9cbb'
       key = Eth::Key.new priv: private_hex
-      expect(key.address).to eq(address.downcase)
+      expect(key.address).to eq address.downcase
       # @TODO Checksummed addresses
-      #expect(key.address).to eq(address)
+      #expect(key.address).to eq address
     end
   end
 end
--- spec/eth/utils_spec.rb
+++ spec/eth/utils_spec.rb
@@ -8,21 +8,21 @@ describe Eth::Utils do
       alice = Eth::Utils.public_key_to_address Eth::Key.new.public_bytes
       expect(alice).to be
       expect(Eth::Utils.is_prefixed? alice).to be_truthy
-      expect(alice.size).to eq(42)
+      expect(alice.size).to eq 42
 
       # same as alice but trying to insert hex
       bob = Eth::Utils.public_key_to_address Eth::Key.new.public_hex
       expect(bob).to be
       expect(Eth::Utils.is_prefixed? bob).to be_truthy
-      expect(bob.size).to eq(42)
+      expect(bob.size).to eq 42
     end
 
     it "turns a hex public key into a hex address" do
       address = "0x8ABC566c5198bc6993526DB697FFe58ce4e2425A"
       public_hex = "0463a1ad6824c03f81ad6c9c224384172c67f6bfd2dbde8c4747a033629b531ae3284db3045e4e40c2b865e22a806ae7dff9264299ea8696321f689d6e134d937e"
-      expect(Eth::Utils.public_key_to_address public_hex).to eq(address.downcase)
+      expect(Eth::Utils.public_key_to_address public_hex).to eq address.downcase
       # @TODO Checksummed addresses
-      # expect(Eth::Utils.public_key_to_address public_hex).to eq(address)
+      # expect(Eth::Utils.public_key_to_address public_hex).to eq address
     end
   end
 
@@ -33,14 +33,14 @@ describe Eth::Utils do
       # ref https://ethereum.stackexchange.com/a/560
       expected_hash = "\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"
       keccak_hex = "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
-      actual_hash = Eth::Utils.keccak256('')
+      actual_hash = Eth::Utils.keccak256 ''
       actual_hex = Eth::Utils.bin_to_hex actual_hash
-      expect(actual_hash).to eq(expected_hash)
-      expect(actual_hex).to eq(keccak_hex)
+      expect(actual_hash).to eq expected_hash
+      expect(actual_hex).to eq keccak_hex
 
       # ensures we are not using the final SHA3 FIPS standard
       sha3_hex = "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a"
-      expect(actual_hex).not_to eq(sha3_hex)
+      expect(actual_hex).not_to eq sha3_hex
     end
 
     it "hashes oscar barrett's keccak256 string correctly" do
@@ -49,14 +49,14 @@ describe Eth::Utils do
       # ref https://github.com/OscarBarrett/crystal-sha3/blob/7b6f6e02196b106ecf0be01da207dbf1e269009b/README.md
       expected_hash = "q\x9A\xCC\xC6\x1A\x9C\xC1&\x83\x0EY\x06\xF9\xD6r\xD0n\xABo\x85\x97(p\x95\xA2\xC5Z\x8Bw^p\x16"
       keccak_hex = "719accc61a9cc126830e5906f9d672d06eab6f8597287095a2c55a8b775e7016"
-      actual_hash = Eth::Utils.keccak256('abc123')
+      actual_hash = Eth::Utils.keccak256 'abc123'
       actual_hex = Eth::Utils.bin_to_hex actual_hash
-      expect(actual_hash).to eq(expected_hash)
-      expect(actual_hex).to eq(keccak_hex)
+      expect(actual_hash).to eq expected_hash
+      expect(actual_hex).to eq keccak_hex
 
       # ensures we are not using the final SHA3 FIPS standard
       sha3_hex = "f58fa3df820114f56e1544354379820cff464c9c41cb3ca0ad0b0843c9bb67ee"
-      expect(actual_hex).not_to eq(sha3_hex)
+      expect(actual_hex).not_to eq sha3_hex
     end
   end
 
@@ -67,34 +67,34 @@ describe Eth::Utils do
       prefixed = "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
 
       # convert back and forth between hexa-decimal and binary strings.
-      expect(Eth::Utils.bin_to_hex bin).to eq(hex)
-      expect(Eth::Utils.hex_to_bin hex).to eq(bin)
-      expect(Eth::Utils.bin_to_prefixed_hex bin).to eq(prefixed)
-      expect(Eth::Utils.hex_to_bin prefixed).to eq(bin)
+      expect(Eth::Utils.bin_to_hex bin).to eq hex
+      expect(Eth::Utils.hex_to_bin hex).to eq bin
+      expect(Eth::Utils.bin_to_prefixed_hex bin).to eq prefixed
+      expect(Eth::Utils.hex_to_bin prefixed).to eq bin
     end
 
     it "raises an error when given invalid string" do
-      expect { Eth::Utils.bin_to_hex 1234 }.to raise_error(TypeError)
-      expect { Eth::Utils.hex_to_bin 'xxxx' }.to raise_error(TypeError)
-      expect { Eth::Utils.hex_to_bin "\x00\x00" }.to raise_error(TypeError)
-      expect { Eth::Utils.hex_to_bin 1234 }.to raise_error(TypeError)
+      expect {Eth::Utils.bin_to_hex 1234}.to raise_error TypeError
+      expect {Eth::Utils.hex_to_bin 'xxxx'}.to raise_error TypeError
+      expect {Eth::Utils.hex_to_bin "\x00\x00"}.to raise_error TypeError
+      expect {Eth::Utils.hex_to_bin 1234}.to raise_error TypeError
     end
   end
 
   describe ".prefix_hex .remove_hex_prefix" do
     it "ensures that a hex value has 0x at the beginning" do
-      expect(Eth::Utils.prefix_hex('abc')).to eq('0xabc')
-      expect(Eth::Utils.prefix_hex('0xabc')).to eq('0xabc')
+      expect(Eth::Utils.prefix_hex 'abc').to eq '0xabc'
+      expect(Eth::Utils.prefix_hex '0xabc').to eq '0xabc'
     end
 
     it "ensures we can remove 0x hex prefixes" do
-      expect(Eth::Utils.remove_hex_prefix('abc')).to eq('abc')
-      expect(Eth::Utils.remove_hex_prefix('0xabc')).to eq('abc')
+      expect(Eth::Utils.remove_hex_prefix 'abc').to eq 'abc'
+      expect(Eth::Utils.remove_hex_prefix '0xabc').to eq 'abc'
     end
 
     it "does not reformat the hex or remove leading zeros" do
-      expect(Eth::Utils.prefix_hex('0123')).to eq('0x0123')
-      expect(Eth::Utils.remove_hex_prefix('0x0123')).to eq('0123')
+      expect(Eth::Utils.prefix_hex '0123').to eq '0x0123'
+      expect(Eth::Utils.remove_hex_prefix '0x0123').to eq '0123'
     end
   end
 
