ed6d2f5c5fb700809718ee450827a93e5d746855
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-12-14 18:19:32 +0100

    ci: only run coverage on CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require 'simplecov'
-SimpleCov.start
-
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 require 'eth'
 RSpec.configure do |config|
