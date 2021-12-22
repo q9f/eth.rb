0976777ec8291303e2dad2a3ddf4d70c96dddfa6
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-12-22 16:18:49 +0100

    ci: run rufo on ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
+# use the local version of the code instead of a globally installed gem
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
+# only run coverage reports on github actions
 if ENV["COVERAGE"]
   require "simplecov"
   SimpleCov.start
