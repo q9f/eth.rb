9530740ed562ad38598704697d1abdcb98d6f942
Afr Schoe <58883403+q9f@users.noreply.github.com>
2022-01-05 18:10:54 +0100

    eth/tx: implement eip-2930 type-1 transactions

--- lib/eth/tx/eip2930.rb
+++ lib/eth/tx/eip2930.rb
@@ -20,7 +20,7 @@ module Eth
 
     # Provides legacy support for transactions on blockchains that do not
     # implement EIP-1559 but still want to utilize EIP-2718 envelopes.
-    class Eip2930Payload
+    class Eip2930
 
       # The EIP-155 Chain ID.
       attr_reader :chain_id
@@ -59,38 +59,207 @@ module Eth
       # can be prepared for envelope, signature and broadcast. Should not
       # be used unless there is no EIP-1559 support.
       #
-      # @param price [Integer] the transaction gas price in Wei.
-      # @param limit [Integer] the transaction gas limit.
-      # @param nonce [Integer] the transaction signer nonce.
-      # @param to [Eth::Address] the transaction destination.
-      # @param value [Integer] the transaction amount in Wei.
-      # @param data [String] the transaction hex-string payload.
-      # @param access_list [Array] an optional EIP-2930 access list.
+      # @param params [Hash] all necessary transaction fields (nonce, gas_price, gas_limit, to, value, data_bin, access_list).
       # @param chain_id [Integer] the EIP-155 Chain ID.
-      def initialize(price, limit, nonce, to = "", value = 0, data = "", access_list = [], chain_id = Chain::ETHEREUM)
-        if to.is_a? String and !to.empty?
-          to = Address.new to
-        end
-        unless price >= 0
-          raise ArgumentError, "Invalid gas price #{price}!"
-        end
-        unless limit >= DEFAULT_LIMIT and limit <= BLOCK_LIMIT
-          raise ArgumentError, "Invalid gas limit #{limit}!"
-        end
-        unless nonce >= 0
-          raise ArgumentError, "Invalid signer nonce #{nonce}!"
-        end
-        unless value >= 0
-          raise ArgumentError, "Invalid transaction value #{value}!"
-        end
+      def initialize(params, chain_id = Chain::ETHEREUM)
+        fields = { recovery_id: nil, r: 0, s: 0 }.merge params
+
+        # populate optional fields with serializable empty values
+        fields[:value] = Tx.sanitize_amount fields[:value]
+        fields[:to] = Tx.sanitize_address fields[:to]
+        fields[:data_bin] = Tx.sanitize_data fields[:data_bin]
+
+        # ensure sane values for all mandatory fields
+        fields = Tx.validate_legacy_params fields
+
+        # populate class attributes
+        @signer_nonce = fields[:nonce].to_i
+        @gas_price = fields[:gas_price].to_i
+        @gas_limit = fields[:gas_limit].to_i
+        @destination = fields[:to].to_s
+        @amount = fields[:value].to_i
+        @payload = fields[:data_bin]
+        @access_list = fields[:access_list]
+
+        # the signature v is set to the chain id for unsigned transactions
+        @signature_y_parity = fields[:recovery_id]
         @chain_id = chain_id
-        @signer_nonce = nonce
-        @gas_price = price
-        @gas_limit = limit
-        @destination = to
-        @amount = value
-        @payload = data
+
+        # the signature fields are empty for unsigned transactions.
+        @signature_r = fields[:r]
+        @signature_s = fields[:s]
+      end
+
+      # overloads the constructor for decoding raw transactions and creating unsigned copies
+      konstructor :decode, :unsigned_copy
+
+      # Decodes a raw transaction hex into an Eth::Tx::Eip2930
+      # transaction object.
+      #
+      # @param hex [String] the raw transaction hex-string.
+      # @return [Eth::Tx::Eip2930] transaction payload.
+      def decode(hex)
+        type = hex[0, 2]
+        raise StandardError, "Invalid transaction type #{type}!" if type.to_i(16) != TYPE_2930
+
+        bin = Util.hex_to_bin hex[2..]
+        tx = RLP.decode(bin)
+
+        # decoded transactions always have 8 + 3 fields, even if they are empty or zero
+        raise StandardError, "Transaction missing fields!" if tx.size < 8
+
+        # populate the 8 payload fields
+        chain_id = Util.deserialize_big_endian_to_int tx[0]
+        nonce = Util.deserialize_big_endian_to_int tx[1]
+        gas_price = Util.deserialize_big_endian_to_int tx[2]
+        gas_limit = Util.deserialize_big_endian_to_int tx[3]
+        to = Util.bin_to_hex tx[4]
+        value = Util.deserialize_big_endian_to_int tx[5]
+        data_bin = tx[6]
+        access_list = tx[7]
+
+        # populate class attributes
+        @chain_id = chain_id
+        @signer_nonce = nonce.to_i
+        @gas_price = gas_price.to_i
+        @gas_limit = gas_limit.to_i
+        @destination = to.to_s
+        @amount = value.to_i
+        @payload = data_bin
         @access_list = access_list
+
+        # populate the 3 signature fields
+        if tx.size == 8
+          _set_signature(nil, 0, 0)
+        elsif tx.size == 11
+          recovery_id = Util.bin_to_hex(tx[8]).to_i(16)
+          r = Util.bin_to_hex tx[9]
+          s = Util.bin_to_hex tx[10]
+
+          # allows us to force-setting a signature if the transaction is signed already
+          _set_signature(recovery_id, r, s)
+        else
+          raise_error StandardError "Cannot decode EIP-2930 payload!"
+        end
+      end
+
+      # Creates an unsigned copy of a transaction payload.
+      #
+      # @param tx [Eth::Tx::Eip2930] an EIP-2930 transaction payload.
+      # @return [Eth::Tx::Eip2930] an unsigned EIP-2930 transaction payload.
+      def unsigned_copy(tx)
+
+        # not checking transaction validity unless it's of a different class
+        raise ArgumentError "Cannot copy transaction of different payload type!" unless tx.instance_of? Tx::Eip2930
+
+        # populate class attributes
+        @signer_nonce = tx.signer_nonce
+        @gas_price = tx.gas_price
+        @gas_limit = tx.gas_limit
+        @destination = tx.destination
+        @amount = tx.amount
+        @payload = tx.payload
+        @access_list = tx.access_list
+        @chain_id = tx.chain_id
+
+        # force-set signature to unsigned
+        _set_signature(nil, 0, 0)
+      end
+
+      # Sign the transaction with a given key.
+      #
+      # @param key [Eth::Key] the key-pair to use for signing.
+      # @raise [StandardError] if the transaction is already signed.
+      def sign(key)
+        if Tx.is_signed? self
+          raise StandardError, "Transaction is already signed!"
+        end
+
+        # sign a keccak hash of the unsigned, encoded transaction
+        signature = key.sign(unsigned_hash, @chain_id)
+        r, s, v = Signature.dissect signature
+        recovery_id = Chain.to_recovery_id v.to_i(16), @chain_id
+        @signature_y_parity = recovery_id
+        @signature_r = r
+        @signature_s = s
+      end
+
+      # Encodes a raw transaction object, wraps it in an EIP-2718 envelope
+      # with an EIP-2930 type prefix.
+      #
+      # @return [String] a raw, RLP-encoded EIP-2930 type transaction object.
+      # @raise [StandardError] if the transaction is not yet signed.
+      def encoded
+        unless Tx.is_signed? self
+          raise StandardError, "Transaction is not signed!"
+        end
+        tx_data = []
+        tx_data.push Util.serialize_int_to_big_endian @chain_id
+        tx_data.push Util.serialize_int_to_big_endian @signer_nonce
+        tx_data.push Util.serialize_int_to_big_endian @gas_price
+        tx_data.push Util.serialize_int_to_big_endian @gas_limit
+        tx_data.push Util.hex_to_bin @destination
+        tx_data.push Util.serialize_int_to_big_endian @amount
+        tx_data.push @payload
+        tx_data.push @access_list
+        tx_data.push Util.serialize_int_to_big_endian @signature_y_parity
+        tx_data.push Util.hex_to_bin @signature_r
+        tx_data.push Util.hex_to_bin @signature_s
+        tx_encoded = RLP.encode tx_data
+
+        # create an EIP-2718 envelope with EIP-2930 type payload
+        tx_type = Util.serialize_int_to_big_endian TYPE_2930
+        return "#{tx_type}#{tx_encoded}"
+      end
+
+      # Gets the encoded, enveloped, raw transaction hex.
+      #
+      # @return [String] the raw transaction hex.
+      def hex
+        Util.bin_to_hex encoded
+      end
+
+      # Gets the transaction hash.
+      #
+      # @return [String] the transaction hash.
+      def hash
+        Util.bin_to_hex Util.keccak256 encoded
+      end
+
+      # Encodes the unsigned transaction payload in an EIP-2930 envelope,
+      # required for signing.
+      #
+      # @return [String] an RLP-encoded, unsigned, enveloped EIP-2930 transaction.
+      def unsigned_encoded
+        tx_data = []
+        tx_data.push Util.serialize_int_to_big_endian @chain_id
+        tx_data.push Util.serialize_int_to_big_endian @signer_nonce
+        tx_data.push Util.serialize_int_to_big_endian @gas_price
+        tx_data.push Util.serialize_int_to_big_endian @gas_limit
+        tx_data.push Util.hex_to_bin @destination
+        tx_data.push Util.serialize_int_to_big_endian @amount
+        tx_data.push @payload
+        tx_data.push @access_list
+        tx_encoded = RLP.encode tx_data
+
+        # create an EIP-2718 envelope with EIP-2930 type payload (unsigned)
+        tx_type = Util.serialize_int_to_big_endian TYPE_2930
+        return "#{tx_type}#{tx_encoded}"
+      end
+
+      # Gets the sign-hash required to sign a raw transaction.
+      #
+      # @return [String] a Keccak-256 hash of an unsigned transaction.
+      def unsigned_hash
+        Util.keccak256 unsigned_encoded
+      end
+
+      private
+
+      def _set_signature(recovery_id, r, s)
+        @signature_y_parity = recovery_id
+        @signature_r = r
+        @signature_s = s
       end
     end
   end
--- spec/eth/tx/eip2930_spec.rb
+++ spec/eth/tx/eip2930_spec.rb
@@ -1,19 +1,120 @@
 require "spec_helper"
 
-describe Eth::Tx::Eip2930Payload do
+describe Eth::Tx::Eip2930 do
+  subject(:list) {
+    [
+      [
+        "0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae",
+        [
+          "0x0000000000000000000000000000000000000000000000000000000000000003",
+          "0x0000000000000000000000000000000000000000000000000000000000000007",
+        ],
+      ],
+      [
+        "0xbb9bc244d798123fde783fcc1c72d3bb8c189413",
+        [],
+      ],
+    ]
+  }
+
   describe ".initialize" do
     it "creates EIP-2930 transaction payload objects" do
-      expect(Eth::Tx::Eip2930Payload.new(Eth::Unit::GWEI, Eth::Tx::DEFAULT_LIMIT, 0)).to be
-      expect(Eth::Tx::Eip2930Payload.new(Eth::Unit::GWEI, Eth::Tx::DEFAULT_LIMIT, 0)).to be_instance_of Eth::Tx::Eip2930Payload
+      expect(Eth::Tx::Eip2930.new({
+        nonce: 0,
+        gas_price: Eth::Unit::GWEI,
+        gas_limit: Eth::Tx::DEFAULT_LIMIT,
+      })).to be
+      expect(Eth::Tx::Eip2930.new({
+        nonce: 0,
+        gas_price: Eth::Unit::GWEI,
+        gas_limit: Eth::Tx::DEFAULT_LIMIT,
+      })).to be_instance_of Eth::Tx::Eip2930
     end
 
     it "doesn't create invalid transaction payload objects" do
-      expect { Eth::Tx::Eip2930Payload.new(-9 * Eth::Unit::GWEI, Eth::Tx::DEFAULT_LIMIT, 0) }.to raise_error ArgumentError
-      expect { Eth::Tx::Eip2930Payload.new(Eth::Unit::GWEI, Eth::Tx::DEFAULT_LIMIT - 1, 0) }.to raise_error ArgumentError
-      expect { Eth::Tx::Eip2930Payload.new(Eth::Unit::GWEI, Eth::Tx::BLOCK_LIMIT + 1, 0) }.to raise_error ArgumentError
-      expect { Eth::Tx::Eip2930Payload.new(Eth::Unit::GWEI, Eth::Tx::BLOCK_LIMIT, -1) }.to raise_error ArgumentError
-      expect { Eth::Tx::Eip2930Payload.new(Eth::Unit::GWEI, Eth::Tx::BLOCK_LIMIT, 0, "foo") }.to raise_error ArgumentError
-      expect { Eth::Tx::Eip2930Payload.new(Eth::Unit::GWEI, Eth::Tx::BLOCK_LIMIT, 0, "0xef26b1f67797e7a5a3c192c93d821fadef3ba173", -1) }.to raise_error ArgumentError
+      expect {
+        Eth::Tx::Eip2930.new({
+          nonce: 0,
+          gas_price: -9 * Eth::Unit::GWEI,
+          gas_limit: Eth::Tx::DEFAULT_LIMIT,
+        })
+      }.to raise_error ArgumentError
+      expect {
+        Eth::Tx::Eip2930.new({
+          nonce: 0,
+          gas_price: Eth::Unit::GWEI,
+          gas_limit: Eth::Tx::DEFAULT_LIMIT - 1,
+        })
+      }.to raise_error ArgumentError
+      expect {
+        Eth::Tx::Eip2930.new({
+          nonce: 0,
+          gas_price: Eth::Unit::GWEI,
+          gas_limit: Eth::Tx::BLOCK_LIMIT + 1,
+        })
+      }.to raise_error ArgumentError
+      expect {
+        Eth::Tx::Eip2930.new({
+          nonce: -1,
+          gas_price: Eth::Unit::GWEI,
+          gas_limit: Eth::Tx::BLOCK_LIMIT,
+        })
+      }.to raise_error ArgumentError
+      expect {
+        Eth::Tx::Eip2930.new({
+          nonce: 0,
+          gas_price: Eth::Unit::GWEI,
+          gas_limit: Eth::Tx::BLOCK_LIMIT,
+          to: "foo",
+        })
+      }.to raise_error ArgumentError
+      expect {
+        Eth::Tx::Eip2930.new({
+          nonce: 0,
+          gas_price: Eth::Unit::GWEI,
+          gas_limit: Eth::Tx::BLOCK_LIMIT,
+          to: "0xef26b1f67797e7a5a3c192c93d821fadef3ba173",
+          value: -1,
+        })
+      }.to raise_error ArgumentError
+      expect {
+        Eth::Tx::Eip2930.new({
+          nonce: 0,
+          gas_price: Eth::Unit::GWEI,
+          gas_limit: Eth::Tx::BLOCK_LIMIT,
+          to: "0xef26b1f67797e7a5a3c192c93d821fadef3ba173",
+          value: 1,
+          access_list: "bar",
+        })
+      }.to raise_error ArgumentError
+    end
+  end
+
+  context "signing transactions the hard way" do
+    it "correctly hashes an unsigned example" do
+      sample = Eth::Tx::Eip2930.new({
+        nonce: 0,
+        gas_price: 0x0BA43B7400,
+        gas_limit: 0x05208,
+        to: "0x7917bc33eea648809c285607579c9919fb864f8f",
+        value: 0x03BAF82D03A000,
+        access_list: list,
+      })
+      lsong = Eth::Key.new(priv: "00d862c318d05de0a1c25242c21989e15e35e70c55996fbc4238cd2f2f6a8f62")
+      expected_address = Eth::Address.new "8d900bfa2353548a4631be870f99939575551b60"
+
+      # a secp256k1 signature over keccak256(0x01 || rlp([chainId, nonce, gasPrice, gasLimit, to, value, data, accessList]))
+      expected_sign_data = "01f9010f0180850ba43b7400825208947917bc33eea648809c285607579c9919fb864f8f8703baf82d03a00080f8e4f8b5aa307864653062323935363639613966643933643566323864396563383565343066346362363937626165f888b842307830303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303033b842307830303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303037ecaa307862623962633234346437393831323366646537383366636331633732643362623863313839343133c0"
+      expected_sign_hash = "d5f0a2ce85dc613ba965db24d17ceb73b6d6927eeaae8a56219968e0223eb45b"
+
+      # first byte is type 01 as per EIP-2930
+      expect(Eth::Util.bin_to_hex (sample.unsigned_encoded)[0, 1]).to eq "01"
+      expect(Eth::Util.bin_to_hex sample.unsigned_encoded).to eq expected_sign_data
+      expect(Eth::Util.bin_to_hex sample.unsigned_hash).to eq expected_sign_hash
+
+      sample.sign lsong
+      expect(Eth::Tx::Eip2930.decode(sample.hex).hex).to eq sample.hex
+      expect(Eth::Tx::Eip2930.decode(sample.hex).hash).to eq sample.hash
     end
   end
 end
