dfcc8b0f08143afc411c878e2786c3a2f6586831
Afr Schoe <58883403+q9f@users.noreply.github.com>
2022-01-19 09:51:55 +0100

    eth: move constant module 1 up

---
+++ lib/eth/constant.rb
@@ -0,0 +1,71 @@
+# Copyright (c) 2016-2022 The Ruby-Eth Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#  -*- encoding : ascii-8bit -*-
+
+# Provides the `Eth` module.
+module Eth
+
+  # Provides commonly used constants, such as zero bytes or zero keys.
+  module Constant
+
+    # The empty byte is defined as "".
+    BYTE_EMPTY = "".freeze
+
+    # The zero byte is 0x00.
+    BYTE_ZERO = "\x00".freeze
+
+    # The byte one is 0x01.
+    BYTE_ONE = "\x01".freeze
+
+    # The size of a 32-bit number.
+    TT32 = (2 ** 32).freeze
+
+    # The size of a 256-bit number.
+    TT256 = (2 ** 256).freeze
+
+    # The maximum possible value of an UInt256.
+    UINT_MAX = (2 ** 256 - 1).freeze
+
+    # The minimum possible value of an UInt256.
+    UINT_MIN = 0.freeze
+
+    # The maximum possible value of an Int256.
+    INT_MAX = (2 ** 255 - 1).freeze
+
+    # The minimum possible value of an Int256.
+    INT_MIN = (-2 ** 255).freeze
+
+    # A hash containing only zeros.
+    HASH_ZERO = ("\x00" * 32).freeze
+
+    # A private key containing only zeros.
+    PRIVKEY_ZERO = ("\x00" * 32).freeze
+
+    # A private key containing only zeros (hex).
+    PRIVKEY_ZERO_HEX = ("0" * 64).freeze
+
+    # The RLP short length limit.
+    SHORT_LENGTH_LIMIT = 56.freeze
+
+    # The RLP long length limit.
+    LONG_LENGTH_LIMIT = (256 ** 8).freeze
+
+    # The RLP primitive type offset.
+    PRIMITIVE_PREFIX_OFFSET = 0x80.freeze
+
+    # THe RLP array type offset.
+    LIST_PREFIX_OFFSET = 0xc0.freeze
+  end
+end
---
+++ spec/eth/constant_spec.rb
@@ -0,0 +1,26 @@
+# # -*- encoding : ascii-8bit -*-
+
+require "spec_helper"
+
+describe Eth::Constant do
+  describe "#SYMBOLS" do
+    it "has valid constants" do
+      expect(Constant::BYTE_EMPTY).to eq ""
+      expect(Constant::BYTE_ZERO).to eq "\x00"
+      expect(Constant::BYTE_ONE).to eq "\x01"
+      expect(Constant::TT32).to eq 4294967296
+      expect(Constant::TT256).to eq 115792089237316195423570985008687907853269984665640564039457584007913129639936
+      expect(Constant::UINT_MAX).to eq 115792089237316195423570985008687907853269984665640564039457584007913129639935
+      expect(Constant::UINT_MIN).to eq 0
+      expect(Constant::INT_MAX).to eq 57896044618658097711785492504343953926634992332820282019728792003956564819967
+      expect(Constant::INT_MIN).to eq -57896044618658097711785492504343953926634992332820282019728792003956564819968
+      expect(Constant::HASH_ZERO).to eq "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+      expect(Constant::PRIVKEY_ZERO).to eq "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+      expect(Constant::PRIVKEY_ZERO_HEX).to eq "0000000000000000000000000000000000000000000000000000000000000000"
+      expect(Constant::SHORT_LENGTH_LIMIT).to eq 56
+      expect(Constant::LONG_LENGTH_LIMIT).to eq 18446744073709551616
+      expect(Constant::PRIMITIVE_PREFIX_OFFSET).to eq 128
+      expect(Constant::LIST_PREFIX_OFFSET).to eq 192
+    end
+  end
+end
