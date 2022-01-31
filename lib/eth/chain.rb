334a3e810f9aa6273eb7cb9cd625391b702a3808
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2022-01-31 17:58:00 +0100

    eth/chain: add sepolia chain id

--- lib/eth/chain.rb
+++ lib/eth/chain.rb
@@ -12,82 +12,85 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Provides the `Eth` module.
+# Provides the {Eth} module.
 module Eth
 
-  # Encapsulates `Chain` IDs and utilities for EIP-155 compatibility.
-  # ref: https://eips.ethereum.org/EIPS/eip-155
+  # Encapsulates {Eth::Chain} IDs and utilities for EIP-155 compatibility.
+  # Ref: https://eips.ethereum.org/EIPS/eip-155
   module Chain
     extend self
 
     # Provides a special replay protection error if EIP-155 is violated.
     class ReplayProtectionError < StandardError; end
 
-    # Chain ID for Ethereum mainnet
+    # Chain ID for Ethereum mainnet.
     ETHEREUM = 1.freeze
 
-    # Chain ID for Expanse mainnet
+    # Chain ID for Expanse mainnet.
     EXPANSE = 2.freeze
 
-    # Chain ID for Optimistic Ethereum mainnet
+    # Chain ID for Optimistic Ethereum mainnet.
     OPTIMISM = 10.freeze
 
-    # Chain ID for Ethereum Classic mainnet
+    # Chain ID for Ethereum Classic mainnet.
     CLASSIC = 61.freeze
 
-    # Chain ID for POA Network mainnet
+    # Chain ID for POA Network mainnet.
     POA_NET = 99.freeze
 
-    # Chain ID for xDAI mainnet
+    # Chain ID for Gnosis mainnet.
     XDAI = 100.freeze
 
-    # Chain ID for Arbitrum mainnet
+    # Chain ID for Arbitrum mainnet.
     ARBITRUM = 42161.freeze
 
-    # Chain ID for Morden (Ethereum) testnet
+    # Chain ID for Morden (Ethereum) testnet.
     MORDEN = 2.freeze
 
-    # Chain ID for Ropsten testnet
+    # Chain ID for Ropsten testnet.
     ROPSTEN = 3.freeze
 
-    # Chain ID for Rinkeby testnet
+    # Chain ID for Rinkeby testnet.
     RINKEBY = 4.freeze
 
-    # Chain ID for Goerli testnet
+    # Chain ID for Goerli testnet.
     GOERLI = 5.freeze
 
-    # Chain ID for Kotti testnet
+    # Chain ID for Kotti testnet.
     KOTTI = 6.freeze
 
-    # Chain ID for Kovan testnet
+    # Chain ID for Kovan testnet.
     KOVAN = 42.freeze
 
-    # Chain ID for Morden (Classic) testnet
+    # Chain ID for Morden (Classic) testnet.
     MORDEN_CLASSIC = 62.freeze
 
-    # Chain ID for Mordor testnet
+    # Chain ID for Mordor testnet.
     MORDOR = 63.freeze
 
-    # Chain ID for Optimistik Kovan testnet
+    # Chain ID for Optimistik Kovan testnet.
     KOVAN_OPTIMISM = 69.freeze
 
-    # Chain ID for Arbitrum xDAI testnet
+    # Chain ID for Arbitrum xDAI testnet.
     XDAI_ARBITRUM = 200.freeze
 
-    # Chain ID for Optimistic Goerli testnet
+    # Chain ID for Optimistic Goerli testnet.
     GOERLI_OPTIMISM = 420.freeze
 
-    # Chain ID for Arbitrum Rinkeby testnet
+    # Chain ID for Arbitrum Rinkeby testnet.
     RINKEBY_ARBITRUM = 421611.freeze
 
-    # Chain ID for the geth private network preset
+    # Chain ID for Sepolia testnet.
+    SEPOLIA = 11155111.freeze
+
+    # Chain ID for the geth private network preset.
     PRIVATE_GETH = 1337.freeze
 
     # Indicates wether the given `v` indicates a legacy chain value
     # without EIP-155 replay protection.
     #
-    # @param v [Integer] the signature's `v` value
-    # @return [Boolean] true if legacy value
+    # @param v [Integer] the signature's `v` value.
+    # @return [Boolean] true if legacy value.
     def is_legacy?(v)
       [27, 28].include? v
     end
@@ -95,7 +98,7 @@ module Eth
     # Convert a given `v` value to an ECDSA recovery id for the given
     # EIP-155 chain ID.
     #
-    # @param v [Integer] the signature's `v` value
+    # @param v [Integer] the signature's `v` value.
     # @param chain_id [Integer] the chain id the signature was generated on.
     # @return [Integer] the recovery id corresponding to `v`.
     # @raise [ReplayProtectionError] if the given `v` is invalid.
@@ -134,10 +137,10 @@ module Eth
     end
 
     # Converst a `v` value into a chain ID. This does not work for legacy signatures
-    # with v < 36 that do not conform with EIP-155.
+    # with `v < 36` that do not conform with EIP-155.
     #
     # @param v [Integer] the signature's `v` value.
-    # @return [Integer] the chain id as per EIP-155 or nil if there is no replay protection.
+    # @return [Integer] the chain id as per EIP-155 or `nil` if there is no replay protection.
     def to_chain_id(v)
       return nil if v < 36
       chain_id = (v - 35) / 2
