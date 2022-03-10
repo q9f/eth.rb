9d8ea7112f654cc323fd39e0bde05d34b2cd7bbe
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2022-03-10 16:38:52 +0100

    eth/solidity: test deploying a deposit contract

--- spec/eth/solidity_spec.rb
+++ spec/eth/solidity_spec.rb
@@ -27,6 +27,31 @@ describe Solidity do
     expect(result["Greeter"]["bin"]).to start_with "608060405234801561001057600080fd5b5060"
   end
 
+  it "deploys an ethereum-consensus deposit contract" do
+    geth = Client.create "/tmp/geth.ipc"
+    contract = "#{Dir.pwd}/spec/fixtures/contracts/deposit.sol"
+    result = solc.compile contract
+    expect(result["DepositContract"]).to be
+    payload = result["DepositContract"]["bin"]
+    expect(payload).to start_with "60806040523480156200001157600080fd5b5060"
+    params = {
+      from: geth.default_account,
+      priority_fee: 0,
+      max_gas_fee: Unit::GWEI,
+      gas_limit: Tx.estimate_intrinsic_gas(payload),
+      data: payload
+    }
+    deploy = geth.eth_send_transaction(params)
+    hash = deploy["result"]
+    expect(hash).to start_with "0x"
+    geth.wait_for_tx hash
+    receipt = geth.eth_get_transaction_receipt hash
+    expect(receipt["result"]).to be
+    address = receipt["result"]["contractAddress"]
+    expect(address).to be
+    expect{Address.new address}.not_to raise_error
+  end
+
   it "handles file-system errors" do
     contract = "#{Dir.pwd}/spec/fixtures/contracts/null.sol"
     expect { solc.compile contract }.to raise_error Errno::ENOENT, /No such file or directory - Contract file not found:/
