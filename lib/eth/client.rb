45ac899f5958e45d9819bf057b0c0ce08514c851
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2022-05-06 21:21:31 +0900

    Update lib/eth/client.rb
    Co-authored-by: Afr Schoe <58883403+q9f@users.noreply.github.com>

--- lib/eth/client.rb
+++ lib/eth/client.rb
@@ -150,7 +150,7 @@ module Eth
     end
 
     # Deploy contract and waits for it to be mined.
-    # Uses `eth_coinbase` and external signer
+    # Uses `eth_coinbase` or external signer
     # if no sender key is provided.
     #
     # @overload deploy(contract)
