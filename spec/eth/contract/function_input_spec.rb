a9de6570a06f46ca346df4f81542aedd95dd714c
Masumi Kawasaki <geeknees@gmail.com>
2022-09-26 22:25:40 +0900

    Fixed to return uint256[] correctly when passed as type (#147)

--- spec/eth/contract/function_input_spec.rb
+++ spec/eth/contract/function_input_spec.rb
@@ -7,4 +7,13 @@ describe Contract::FunctionInput do
     expect(function_input.type).to eq("uint256")
     expect(function_input).to be_instance_of Contract::FunctionInput
   end
+
+  context "with an array type" do
+    subject(:function_with_array) { Contract::FunctionInput.new({ "name" => "array", "type" => "uint256[]" }) }
+    it "creates FunctionInput objects" do
+      expect(function_with_array.name).to eq("array")
+      expect(function_with_array.type).to eq("uint256[]")
+      expect(function_with_array).to be_instance_of Contract::FunctionInput
+    end
+  end
 end
--- spec/eth/contract/function_output_spec.rb
+++ spec/eth/contract/function_output_spec.rb
@@ -7,4 +7,13 @@ describe Contract::FunctionOutput do
     expect(function_output.type).to eq("uint256")
     expect(function_output).to be_instance_of Contract::FunctionOutput
   end
+
+  context "with an array type" do
+    subject(:function_with_array) { Contract::FunctionOutput.new({ "name" => "array", "type" => "uint256[]" }) }
+    it "creates FunctionInput objects" do
+      expect(function_with_array.name).to eq("array")
+      expect(function_with_array.type).to eq("uint256[]")
+      expect(function_with_array).to be_instance_of Contract::FunctionOutput
+    end
+  end
 end
