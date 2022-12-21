d57eb470e62273a15ae7b5da62ea38b2b301669e
Afri <58883403+q9f@users.noreply.github.com>
2022-12-21 16:01:16 +0100

    docs: update changelog for 0.5.8 (#180)

--- spec/eth_spec.rb
+++ spec/eth_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
 describe Eth do
-  it "0.5.8 works" do
+  it "0.5.9 works" do
 
     # placeholder to set up spec in future
-    expect(Eth::VERSION).to eq "0.5.8"
+    expect(Eth::VERSION).to eq "0.5.9"
   end
 end
