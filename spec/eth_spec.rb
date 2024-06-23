92a9008cb184f78da2a02480912c6d9d5856ecf5
Afr Schoe <58883403+q9f@users.noreply.github.com>
2024-06-23 23:11:12 +0200

    fix spec

--- spec/eth_spec.rb
+++ spec/eth_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
 describe Eth do
-  it "0.5.11 works" do
+  it "0.5.12 works" do
 
     # placeholder to set up spec in future
-    expect(Eth::VERSION).to eq "0.5.11"
+    expect(Eth::VERSION).to eq "0.5.12"
   end
 end
