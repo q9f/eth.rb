32f1c6632ff229a3c9551548e9e9c49b023f03b2
Afri <58883403+q9f@users.noreply.github.com>
2024-12-17 18:21:37 +0100

    gem: bump version to 0.5.14 (#305)

--- spec/eth_spec.rb
+++ spec/eth_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
 describe Eth do
-  it "0.5.13 works" do
+  it "0.5.14 works" do
 
     # placeholder to set up spec in future
-    expect(Eth::VERSION).to eq "0.5.13"
+    expect(Eth::VERSION).to eq "0.5.14"
   end
 end
