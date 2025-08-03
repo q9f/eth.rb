2b30a1d4af3dcd28ab770d1be07a8725f585014c
Afri <58883403+q9f@users.noreply.github.com>
2025-08-03 22:14:07 +0200

    Allow hex strings in int_to_big_endian

--- lib/eth/util.rb
+++ lib/eth/util.rb
@@ -129,7 +129,11 @@ module Eth
     # @param num [Integer] integer to be converted.
     # @return [String] packed, big-endian integer string.
     def int_to_big_endian(num)
-      hex = num.to_s(16) unless hex? num
+      hex = if hex? num
+        remove_hex_prefix num
+      else
+        num.to_s(16)
+      end
       hex = "0#{hex}" if hex.size.odd?
       hex_to_bin hex
     end
