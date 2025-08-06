16e05b34cb616eb7312341c09c52334a7deff63e
l5y <220195275+l5yth@users.noreply.github.com>
2025-08-06 16:16:08 +0200

    chore: bump version to 0.5.16

--- spec/eth_spec.rb
+++ spec/eth_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
 describe Eth do
-  it "0.5.15 works" do
+  it "0.5.16 works" do
 
     # placeholder to set up spec in future
-    expect(Eth::VERSION).to eq "0.5.15"
+    expect(Eth::VERSION).to eq "0.5.16"
   end
 end
