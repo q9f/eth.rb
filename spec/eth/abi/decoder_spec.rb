2fc19d9ec62362a78d98f84fb288bf65f3e95cb0
Afri <58883403+q9f@users.noreply.github.com>
2025-08-06 13:48:57 +0200

    Move ZST regression test into decoder spec

--- spec/eth/abi/decoder_spec.rb
+++ spec/eth/abi/decoder_spec.rb
@@ -83,4 +83,15 @@ describe Abi::Decoder do
     # uncovered edge-cases
     expect { Abi::Decoder.primitive_type(Abi::Type.new("foo", 32, []), "bar") }.to raise_error Abi::DecodingError
   end
+
+  describe "ZST robustness" do
+    it "rejects self-referential dynamic array offsets" do
+      payload = "0000000000000000000000000000000000000000000000000000000000000020" \
+                "0000000000000000000000000000000000000000000000000000000000000002" \
+                "0000000000000000000000000000000000000000000000000000000000000020" \
+                "0000000000000000000000000000000000000000000000000000000000000020"
+      data = Util.hex_to_bin(payload)
+      expect { Abi.decode(["uint256[][]"], data) }.to raise_error Abi::DecodingError
+    end
+  end
 end
